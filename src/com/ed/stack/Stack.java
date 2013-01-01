f1a6075c43aa1774901895dfc8a170f0a94eef6a
Bruno Basto <bruno.basto@liferay.com>
2012-12-31 21:25:51 -0300

    Adding Stack

--- src/com/ed/stack/Stack.java
+++ src/com/ed/stack/Stack.java
@@ -1,5 +1,24 @@
 package com.ed.stack;
 
+import com.ed.linkedlist.LinkedList;
 public class Stack {
+
+	public Stack() {
+		list = new LinkedList();
+	}
+
+	public Object pop() {
+		return list.remove(size() - 1);
+	}
+
+	public void push(Object value) {
+		list.add(value);
+	}
+
+	public int size() {
+		return list.size();
+	}
+
+	private LinkedList list;
 
 }
