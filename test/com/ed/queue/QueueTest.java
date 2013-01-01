70e313bfa376192559a430b75f02070c9d28e039
Bruno Basto <bruno.basto@liferay.com>
2012-12-31 21:25:42 -0300

    Adding tests for Queue

--- test/com/ed/queue/QueueTest.java
+++ test/com/ed/queue/QueueTest.java
@@ -5,8 +5,25 @@ import org.junit.Test;
 public class QueueTest {
 
 	@Test
-	public void test() {
-		Assert.fail("Not yet implemented");
+	public void testDequeue() {
+		Queue queue = new Queue();
+
+		queue.enqueue("a");
+		queue.enqueue("b");
+
+		Assert.assertEquals("a", queue.dequeue());
+
+		Assert.assertEquals(1, queue.size());
+	}
+
+	@Test
+	public void testEnqueue() {
+		Queue queue = new Queue();
+
+		queue.enqueue("a");
+		queue.enqueue("b");
+
+		Assert.assertEquals(2, queue.size());
 	}
 
 }
